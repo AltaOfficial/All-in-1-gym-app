f263ad22b0c6946ec0fed18d550e8bbc19e46f14
jaedon farr <jaedonfarr@gmail.com>
2025-08-12 04:52:30 -0400

    Add JDBC support and migrate config to YAML
    Added spring-boot-starter-jdbc dependency and updated AppApplication to use DataSource and JdbcTemplate. Migrated application configuration from properties to YAML files, including environment-specific datasource settings. Updated Docker Compose to expose Postgres port. Added initial schema.sql for database setup.

--- services/backend/src/main/java/com/strive/app/AppApplication.java
+++ services/backend/src/main/java/com/strive/app/AppApplication.java
@@ -1,14 +1,29 @@
 package com.strive.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class AppApplication {
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class AppApplication implements CommandLineRunner {
+    private final DataSource dataSource;
+
+    public AppApplication(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) {
+        System.out.println("hello");
+        final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+        restTemplate.execute("select 1");
+    }
 }
