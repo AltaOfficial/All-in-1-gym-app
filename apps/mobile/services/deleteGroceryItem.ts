34d9589a6364f61754833cd1572e37d1feb12957
jaedon farr <jaedonfarr@gmail.com>
2025-09-10 13:45:30 -0400

    Update deleteGroceryItem.ts
    deleting groceries got bugged, this is the fix, now uses item.dateFrom and dateTo, instead of creating a dateFrom and to

--- apps/mobile/services/deleteGroceryItem.ts
+++ apps/mobile/services/deleteGroceryItem.ts
@@ -1,36 +1,47 @@
 import { GroceryListItemType } from "../types/groceryListItemType";
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 
-export async function deleteGroceryItem(item: GroceryListItemType, dateFrom: Date, dateTo: Date, groceryList: GroceryListItemType[]) {
-    console.log("deleting item:", item, dateFrom, dateTo, groceryList);
-    try {
-      console.log("deleting item", item);
-      const response = await fetch(`${process.env.EXPO_PUBLIC_BACKEND_URL}/grocerylist/remove`, {
-        method: 'POST',
+export async function deleteGroceryItem(
+  item: GroceryListItemType,
+  dateFrom: Date,
+  dateTo: Date,
+  groceryList: GroceryListItemType[]
+) {
+  console.log("deleting item:", item, dateFrom, dateTo, groceryList);
+  try {
+    console.log("deleting item", item);
+
+    const response = await fetch(
+      `${process.env.EXPO_PUBLIC_BACKEND_URL}/grocerylist/remove`,
+      {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${await SecureStore.getItemAsync('jwtToken')}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${await SecureStore.getItemAsync("jwtToken")}`,
         },
         body: JSON.stringify({
-          dateFrom: dateFrom?.toISOString(),
-          dateTo: dateTo?.toISOString(),
+          dateFrom: item.groceryListEntityIdDateFrom,
+          dateTo: item.groceryListEntityIdDateTo,
           groceryListItemDto: {
             id: item.id,
             itemName: item.itemName,
             quantity: item.quantity,
             cost: item.cost,
-            isBought: item.isBought
-          }
+            isBought: item.isBought,
+          },
         }),
-      });
-      
-      if (response.ok) {
-        return groceryList.filter((listItem: GroceryListItemType) => listItem.id !== item.id);
-      } else {
-        return null;
       }
-    } catch (error) {
-      console.error('Error deleting grocery item:', error);
+    );
+
+    if (response.ok) {
+      return groceryList.filter(
+        (listItem: GroceryListItemType) => listItem.id !== item.id
+      );
+    } else {
       return null;
     }
-  };
+  } catch (error) {
+    console.error("Error deleting grocery item:", error);
+    return null;
+  }
+}
